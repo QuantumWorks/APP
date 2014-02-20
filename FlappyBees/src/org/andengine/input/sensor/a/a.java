a47243f8256cd921dadf3e69526829557760ce2e
Syriamanal <Shadinachat123@gmail.com>
2014-02-20 15:32:07 -0800

    Create a.java

---
+++ FlappyBees/src/org/andengine/input/sensor/a/a.java
@@ -0,0 +1,33 @@
+package org.andengine.input.sensor.a;
+
+import java.util.Arrays;
+
+// Referenced classes of package org.andengine.input.sensor.a:
+//            f, b, c, d, 
+//            e
+
+public class a extends org.andengine.input.sensor.a
+{
+
+    private static final f d[];
+
+    public void a(float af[])
+    {
+        super.a(af);
+        d[c].a(a);
+    }
+
+    public String toString()
+    {
+        return (new StringBuilder("Acceleration: ")).append(Arrays.toString(a)).toString();
+    }
+
+    static 
+    {
+        d = new f[4];
+        d[0] = new b();
+        d[1] = new c();
+        d[2] = new d();
+        d[3] = new e();
+    }
+}
