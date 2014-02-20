7e3cf59d2cd59453d3ab5cd55ec0454400ba75b8
Syriamanal <Shadinachat123@gmail.com>
2014-02-20 15:32:40 -0800

    Create c.java

---
+++ FlappyBees/src/org/andengine/input/sensor/a/c.java
@@ -0,0 +1,22 @@
+package org.andengine.input.sensor.a;
+
+
+// Referenced classes of package org.andengine.input.sensor.a:
+//            f
+
+class c
+    implements f
+{
+
+    c()
+    {
+    }
+
+    public void a(float af[])
+    {
+        float f1 = af[1];
+        float f2 = af[0];
+        af[0] = f1;
+        af[1] = f2;
+    }
+}
