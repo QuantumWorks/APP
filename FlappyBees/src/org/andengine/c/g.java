143a77b789686267a941d94c947f42e95f64100b
Syriamanal <Shadinachat123@gmail.com>
2014-02-20 15:07:02 -0800

    Create g.java

---
+++ FlappyBees/src/org/andengine/c/g.java
@@ -0,0 +1,30 @@
+package org.andengine.b.c;
+
+
+public class g
+{
+
+    private boolean a;
+    private long b;
+
+    public g()
+    {
+        b = 20L;
+    }
+
+    public g a(boolean flag)
+    {
+        a = flag;
+        return this;
+    }
+
+    public boolean a()
+    {
+        return a;
+    }
+
+    public long b()
+    {
+        return b;
+    }
+}
