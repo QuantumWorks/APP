80fd4105c71f7512b735aaf630090eb7df376cb8
Syriamanal <Shadinachat123@gmail.com>
2014-02-20 15:42:04 -0800

    Create b.java

---
+++ FlappyBees/src/org/andengine/opengl/b/b/b.java
@@ -0,0 +1,23 @@
+package org.andengine.opengl.b.b;
+
+import org.andengine.opengl.util.e;
+
+// Referenced classes of package org.andengine.opengl.b.b:
+//            a
+
+public class b
+    implements a
+{
+
+    private final String a;
+
+    public b(String s)
+    {
+        a = s;
+    }
+
+    public String a(e e)
+    {
+        return a;
+    }
+}
