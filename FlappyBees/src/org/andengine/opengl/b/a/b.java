13dd589800fb47bcaccc5b28eb942f26d2e181c5
Syriamanal <Shadinachat123@gmail.com>
2014-02-20 15:40:40 -0800

    Create b.java

---
+++ FlappyBees/src/org/andengine/opengl/b/a/b.java
@@ -0,0 +1,17 @@
+package org.andengine.opengl.b.a;
+
+import org.andengine.d.f.a;
+
+public class b extends a
+{
+
+    public b(String s)
+    {
+        super(s);
+    }
+
+    public b(String s, b b1)
+    {
+        super(s, b1);
+    }
+}
