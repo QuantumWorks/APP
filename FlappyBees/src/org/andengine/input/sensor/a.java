5185119db91d32fd9bf2ab83c986d5ea4df5a5c8
Syriamanal <Shadinachat123@gmail.com>
2014-02-20 15:37:26 -0800

    Create a.java

---
+++ FlappyBees/src/org/andengine/input/sensor/a.java
@@ -0,0 +1,26 @@
+package org.andengine.input.sensor;
+
+import java.util.Arrays;
+
+public class a
+{
+
+    protected final float a[];
+    protected int b;
+    protected int c;
+
+    public void a(int i)
+    {
+        b = i;
+    }
+
+    public void a(float af[])
+    {
+        System.arraycopy(af, 0, a, 0, af.length);
+    }
+
+    public String toString()
+    {
+        return (new StringBuilder("Values: ")).append(Arrays.toString(a)).toString();
+    }
+}
