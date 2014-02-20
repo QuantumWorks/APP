09837c8a0facdc921d3921a5c29e9997c24d8853
Syriamanal <Shadinachat123@gmail.com>
2014-02-20 15:35:43 -0800

    Create a.java

---
+++ FlappyBees/src/org/andengine/input/sensor/location/a.java
@@ -0,0 +1,21 @@
+package org.andengine.input.sensor.location;
+
+import android.location.Location;
+import android.os.Bundle;
+
+// Referenced classes of package org.andengine.input.sensor.location:
+//            b
+
+public interface a
+{
+
+    public abstract void a();
+
+    public abstract void a(Location location);
+
+    public abstract void a(b b1, Bundle bundle);
+
+    public abstract void b();
+
+    public abstract void c();
+}
