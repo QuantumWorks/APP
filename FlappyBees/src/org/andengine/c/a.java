d2223afbde6ac3265082507d6be86bea3d9e3ab1
Syriamanal <Shadinachat123@gmail.com>
2014-02-20 15:04:34 -0800

    Create a.java

---
+++ FlappyBees/src/org/andengine/c/a.java
@@ -0,0 +1,45 @@
+package org.andengine.b.c;
+
+
+// Referenced classes of package org.andengine.b.c:
+//            f, c
+
+public class a
+{
+
+    private f a;
+    private c b;
+
+    public a()
+    {
+        a = new f();
+        b = new c();
+    }
+
+    public a a(boolean flag)
+    {
+        a.a(flag);
+        return this;
+    }
+
+    public f a()
+    {
+        return a;
+    }
+
+    public a b(boolean flag)
+    {
+        b.a(flag);
+        return this;
+    }
+
+    public boolean b()
+    {
+        return a.a();
+    }
+
+    public boolean c()
+    {
+        return b.a();
+    }
+}
