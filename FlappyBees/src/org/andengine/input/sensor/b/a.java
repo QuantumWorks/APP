9a15a784975c4ebbe5633217eba0bfc90645b746
Syriamanal <Shadinachat123@gmail.com>
2014-02-20 15:34:48 -0800

    Create a.java

---
+++ FlappyBees/src/org/andengine/input/sensor/b/a.java
@@ -0,0 +1,13 @@
+package org.andengine.input.sensor.b;
+
+
+// Referenced classes of package org.andengine.input.sensor.b:
+//            b
+
+public interface a
+{
+
+    public abstract void a(b b1);
+
+    public abstract void b(b b1);
+}
