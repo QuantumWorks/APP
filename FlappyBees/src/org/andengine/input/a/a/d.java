d7f323520fee918efbebdf166d83c138530b9e8a
Syriamanal <Shadinachat123@gmail.com>
2014-02-20 15:29:03 -0800

    Create d.java

---
+++ FlappyBees/src/org/andengine/input/a/a/d.java
@@ -0,0 +1,16 @@
+package org.andengine.input.a.a;
+
+import android.view.MotionEvent;
+import org.andengine.b.b.c;
+
+// Referenced classes of package org.andengine.input.a.a:
+//            e
+
+public interface d
+    extends c
+{
+
+    public abstract void a(MotionEvent motionevent);
+
+    public abstract void a(e e);
+}
