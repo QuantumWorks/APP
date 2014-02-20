35bcadab40a685fe274a8ce8a05af53e95ffa7bf
Syriamanal <Shadinachat123@gmail.com>
2014-02-20 15:05:19 -0800

    Create c.java

---
+++ FlappyBees/src/org/andengine/c/c.java
@@ -0,0 +1,23 @@
+package org.andengine.b.c;
+
+
+public class c
+{
+
+    private boolean a;
+
+    public c()
+    {
+    }
+
+    public c a(boolean flag)
+    {
+        a = flag;
+        return this;
+    }
+
+    public boolean a()
+    {
+        return a;
+    }
+}
