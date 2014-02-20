18e0e787612c8de140edbe44802e3abeeb0efdfe
Syriamanal <Shadinachat123@gmail.com>
2014-02-20 15:05:37 -0800

    Create d.java

---
+++ FlappyBees/src/org/andengine/c/d.java
@@ -0,0 +1,30 @@
+package org.andengine.b.c;
+
+
+public class d
+{
+
+    private boolean a;
+    private boolean b;
+
+    public d()
+    {
+        a = false;
+        b = false;
+    }
+
+    public void a(boolean flag)
+    {
+        a = flag;
+    }
+
+    public boolean a()
+    {
+        return a;
+    }
+
+    public boolean b()
+    {
+        return b;
+    }
+}
