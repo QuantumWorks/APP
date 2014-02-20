b341cb9ed1985bba9047290a5f5ac19061325580
Syriamanal <Shadinachat123@gmail.com>
2014-02-20 15:08:39 -0800

    Create b.java

---
+++ FlappyBees/src/org/andengine/b.java
@@ -0,0 +1,17 @@
+package org.andengine.b;
+
+
+// Referenced classes of package org.andengine.b:
+//            a
+
+public class b extends InterruptedException
+{
+
+    final a a;
+
+    public b(a a1)
+    {
+        a = a1;
+        super();
+    }
+}
