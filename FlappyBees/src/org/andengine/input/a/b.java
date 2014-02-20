3f6c19636f447cf2d880e29cca62717d7a2a6b1d
Syriamanal <Shadinachat123@gmail.com>
2014-02-20 15:30:37 -0800

    Create b.java

---
+++ FlappyBees/src/org/andengine/input/a/b.java
@@ -0,0 +1,29 @@
+package org.andengine.input.a;
+
+import org.andengine.d.a.b.a;
+
+// Referenced classes of package org.andengine.input.a:
+//            a
+
+final class b extends a
+{
+
+    private b()
+    {
+    }
+
+    b(b b1)
+    {
+        this();
+    }
+
+    protected org.andengine.input.a.a a()
+    {
+        return new org.andengine.input.a.a();
+    }
+
+    protected Object b()
+    {
+        return a();
+    }
+}
