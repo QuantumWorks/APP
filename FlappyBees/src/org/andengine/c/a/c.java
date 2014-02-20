c0f7fb15a29b5e95550847c35eb73ed37d5eed04
Syriamanal <Shadinachat123@gmail.com>
2014-02-20 15:02:24 -0800

    Create c.java

---
+++ FlappyBees/src/org/andengine/c/a/c.java
@@ -0,0 +1,9 @@
+package org.andengine.b.c.a;
+
+import org.andengine.opengl.view.RenderSurfaceView;
+
+public interface c
+{
+
+    public abstract void a(RenderSurfaceView rendersurfaceview, int i, int j);
+}
