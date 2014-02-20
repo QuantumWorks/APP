bf620745b3ae2d3c7851b59e3987dc974b82834c
Syriamanal <Shadinachat123@gmail.com>
2014-02-20 15:40:25 -0800

    Create a.java

---
+++ FlappyBees/src/org/andengine/opengl/b/a/a.java
@@ -0,0 +1,14 @@
+package org.andengine.opengl.b.a;
+
+
+// Referenced classes of package org.andengine.opengl.b.a:
+//            b
+
+public class a extends b
+{
+
+    public a(String s, String s1)
+    {
+        super((new StringBuilder("Reason: ")).append(s).append("\nSource:\n##########################\n").append(s1).append("\n##########################").toString());
+    }
+}
