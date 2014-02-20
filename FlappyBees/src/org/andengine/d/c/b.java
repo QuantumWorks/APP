d8e8a5112b8e94ce997b6bf20d01b6825abcd1c7
Syriamanal <Shadinachat123@gmail.com>
2014-02-20 15:17:55 -0800

    Create b.java

---
+++ FlappyBees/src/org/andengine/d/c/b.java
@@ -0,0 +1,49 @@
+package org.andengine.a.c;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.media.SoundPool;
+
+// Referenced classes of package org.andengine.a.c:
+//            a, c
+
+public class b
+{
+
+    private static String a = "";
+
+    public static a a(c c1, Context context, String s)
+    {
+        c1;
+        JVM INSTR monitorenter ;
+        a a1;
+        a1 = new a(c1, c1.c().load(context.getAssets().openFd((new StringBuilder(String.valueOf(a))).append(s).toString()), 1));
+        c1.a(a1);
+        c1;
+        JVM INSTR monitorexit ;
+        return a1;
+        Exception exception;
+        exception;
+        c1;
+        JVM INSTR monitorexit ;
+        throw exception;
+    }
+
+    public static void a()
+    {
+        a("");
+    }
+
+    public static void a(String s)
+    {
+        if (s.endsWith("/") || s.length() == 0)
+        {
+            a = s;
+            return;
+        } else
+        {
+            throw new IllegalStateException("pAssetBasePath must end with '/' or be lenght zero.");
+        }
+    }
+
+}
