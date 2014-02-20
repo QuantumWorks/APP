3aa80926d784151322d3daa5fd9571434cc75d4c
Syriamanal <Shadinachat123@gmail.com>
2014-02-20 15:29:58 -0800

    Create g.java

---
+++ FlappyBees/src/org/andengine/input/a/a/g.java
@@ -0,0 +1,19 @@
+package org.andengine.input.a.a;
+
+import android.view.MotionEvent;
+
+// Referenced classes of package org.andengine.input.a.a:
+//            a
+
+public class g extends a
+{
+
+    public g()
+    {
+    }
+
+    public void a(MotionEvent motionevent)
+    {
+        a(motionevent.getX(), motionevent.getY(), motionevent.getAction(), 0, motionevent);
+    }
+}
